9fcba651f3f57734fcfd86f63e296bc6506ce31e
Petr Klíma <qaxi@users.noreply.github.com>
2021-04-06 17:28:59 +0200

    added facts subset "interfaces" (#20)
    * implement 'interface' facts subset
    
    * unit tests for 'config' facts subset

--- tests/unit/plugins/modules/ciscosmb/test_ciscosmb_facts-SG500-52-K9.py
+++ tests/unit/plugins/modules/ciscosmb/test_ciscosmb_facts-SG500-52-K9.py
@@ -88,44 +88,34 @@ class TestCiscoSMBFactsModuleOnSG50052K9(TestCiscoSMBModule):
             result['ansible_facts']['ansible_net_spacetotal_mb'], 31.4
         )
 
-#
-#     def test_ciscosmb_facts_config(self):
-#         set_module_args(dict(gather_subset='config'))
-#         result = self.execute_module()
-#         self.assertIsInstance(
-#             result['ansible_facts']['ansible_net_config'], str
-#         )
-#
-#     def test_ciscosmb_facts_interfaces(self):
-#         set_module_args(dict(gather_subset='interfaces'))
-#         result = self.execute_module()
-#         self.assertIn(
-#             result['ansible_facts']['ansible_net_all_ipv4_addresses'][0], ['10.37.129.3', '10.37.0.0', '192.168.88.1']
-#         )
-#         self.assertEqual(
-#             result['ansible_facts']['ansible_net_all_ipv6_addresses'], ['fe80::21c:42ff:fe36:5290']
-#         )
-#         self.assertEqual(
-#             result['ansible_facts']['ansible_net_all_ipv6_addresses'][0],
-#             result['ansible_facts']['ansible_net_interfaces']['ether1']['ipv6'][0]['address']
-#         )
-#         self.assertEqual(
-#             len(result['ansible_facts']['ansible_net_interfaces'].keys()), 11
-#         )
-#         self.assertEqual(
-#             len(result['ansible_facts']['ansible_net_neighbors']), 4
-#         )
-#
-#     def test_ciscosmb_facts_interfaces_no_ipv6(self):
-#         fixture = load_fixture(
-#             'ciscosmb_facts/ipv6_address_print_detail_without-paging_no-ipv6'
-#         )
-#         interfaces = self.module.Interfaces(module=self.module)
-#         addresses = interfaces.parse_detail(data=fixture)
-#         result = interfaces.populate_addresses(data=addresses, family='ipv6')
-#
-#         self.assertEqual(result, None)
-#
+    def test_ciscosmb_facts_config(self):
+        set_module_args(dict(gather_subset='config'))
+        result = self.execute_module()
+        self.assertIsInstance(
+            result['ansible_facts']['ansible_net_config'], str
+        )
+
+    def test_ciscosmb_facts_interfaces(self):
+        set_module_args(dict(gather_subset='interfaces'))
+        result = self.execute_module()
+
+        self.assertIn(
+            result['ansible_facts']['ansible_net_all_ipv4_addresses'][0], ['11.30.5.12']
+        )
+        self.assertEqual(
+            result['ansible_facts']['ansible_net_all_ipv6_addresses'], ['fe80::36db:fdff:fe64:5bce']
+        )
+        self.assertEqual(
+            result['ansible_facts']['ansible_net_all_ipv6_addresses'][0],
+            result['ansible_facts']['ansible_net_interfaces']['vlan 1']['ipv6'][0]['address']
+        )
+        self.assertEqual(
+            len(result['ansible_facts']['ansible_net_interfaces'].keys()), 85
+        )
+        self.assertEqual(
+            len(result['ansible_facts']['ansible_net_neighbors']), 9
+        )
+
 #     def test_ciscosmb_facts_routing(self):
 #         set_module_args(dict(gather_subset='routing'))
 #         result = self.execute_module()
