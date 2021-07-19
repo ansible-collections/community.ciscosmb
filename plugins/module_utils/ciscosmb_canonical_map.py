704b3373cfaf4cbe6aab185fa2ac016cf5b29bc3
Petr Klima <qaxi@seznam.cz>
2021-07-19 14:31:48 +0200

    Remove trailing whitespaces

--- plugins/module_utils/ciscosmb_canonical_map.py
+++ plugins/module_utils/ciscosmb_canonical_map.py
@@ -9,7 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
- 
+
 base_interfaces = {
     "ATM": "ATM",
     "AT": "ATM",
