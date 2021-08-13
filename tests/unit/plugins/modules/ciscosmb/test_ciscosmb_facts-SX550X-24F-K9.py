52d06bd2b1dd65bfdebafceda0c7afbe8da2a25e
Petr Klima <qaxi@seznam.cz>
2021-08-13 12:36:47 +0200

    added maxsplit param to str.split()

--- tests/unit/plugins/modules/ciscosmb/test_ciscosmb_facts-SX550X-24F-K9.py
+++ tests/unit/plugins/modules/ciscosmb/test_ciscosmb_facts-SX550X-24F-K9.py
@@ -43,7 +43,7 @@ class TestCiscoSMBFactsModule(TestCiscoSMBModule):
             output = list()
 
             for command in commands:
-                filename = str(command).split(' | ')[0].replace(' ', '_')
+                filename = str(command).split(' | ', 1)[0].replace(' ', '_')
                 output.append(load_fixture('ciscosmb_facts-SX550X-24F-K9-%s' % filename))
             return output
 
