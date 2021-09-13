a0e8988514ce131d343d9e78b447986647c25421
Petr Klíma <qaxi@users.noreply.github.com>
2021-09-13 07:59:25 +0200

    Module dommand does not support check_mode (#45)

--- plugins/modules/command.py
+++ plugins/modules/command.py
@@ -62,8 +62,6 @@ options:
         trying the command again.
     default: 1
     type: int
-notes:
-  - Supports C(check_mode).
 '''
 
 EXAMPLES = """
@@ -142,7 +140,7 @@ def main():
     argument_spec.update(ciscosmb_argument_spec)
 
     module = AnsibleModule(argument_spec=argument_spec,
-                           supports_check_mode=True)
+                           supports_check_mode=False)
 
     result = {'changed': False}
 
